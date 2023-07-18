187089208e34f7807acc8da33581b484d820a8da
Younes Rabeh <rabehyounes9@gmail.com>
2023-07-18 16:49:58 +0200

    Update README.md

--- my_editor/App/Layout/gui_main.py
+++ my_editor/App/Layout/gui_main.py
@@ -3,7 +3,7 @@ from gui_resources import Info, SetUpWindow, UI
 
 app = customtkinter.CTk()  # create CTk window
 window_info = Info(app)  # window start info are fields in info
-window_set_up = SetUpWindow(app)  # init the window
+window_set_up = SetUpWindow(app, window_info)  # init the window
 window_UI = UI(app, window_info)  # make the UI
 
 
--- my_editor/App/Layout/gui_resources.py
+++ my_editor/App/Layout/gui_resources.py
@@ -1,4 +1,4 @@
-import customtkinter
+import customtkinter as tk
 
 class Info:
     #DEFAULT:
@@ -10,16 +10,34 @@ class Info:
         self.SCREEN_HEIGHT = _app.winfo_screenheight()
 
 class SetUpWindow:
-    def __init__(self, _app):
-        customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-        customtkinter.set_default_color_theme("blue")
-        _app.geometry("1300x900")
-        _app.attributes("-zoomed", True)
+    def __init__(self, _app, _inf):
+        tk.set_appearance_mode("dark")  # Modes: system (default), light, dark
+        tk.set_default_color_theme("blue")
+        size = f"{_inf.SCREEN_WIDTH//2}x{_inf.SCREEN_HEIGHT//2}"
+        _app.geometry(size)
+        _app.attributes('-zoomed', True)  # Set the window to full screen
+        #_app.resizable(False, False)
+
 
 class UI:
     def __init__(self, _app, _inf):
-        button = customtkinter.CTkButton(master=_app, text="CTkButton", command=lambda: (self.button_function(_inf)))
-        button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+        # DRAW UI #
+        self.files_frame = tk.CTkFrame(master=_app, fg_color="white",border_color="black",
+                                       corner_radius=0, width=_inf.SCREEN_WIDTH//2, height=_inf.SCREEN_HEIGHT//1.86)
+        self.files_frame.place(relx=0, rely=0)
+        self.window_frame = tk.CTkFrame(master=_app, fg_color="yellow", border_color="black",
+                                        corner_radius=0, width=_inf.SCREEN_WIDTH//2, height=_inf.SCREEN_HEIGHT//1.86)
+        #self.window_frame.place(relx=0.5, rely=0)
+        self.timeline_frame = tk.CTkFrame(master=_app, fg_color="blue", border_color="black",
+                                          corner_radius=0, width=_inf.SCREEN_WIDTH, height=_inf.SCREEN_HEIGHT//2.24)
+        self.timeline_frame.place(relx=0, rely=0.55)
+
+        self.button = tk.CTkButton(master=_app, text="CTkButton", command=lambda: (self.button_function(_inf)))
+        self.button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+        self.my_text = tk.CTkLabel(master=self.files_frame, text=" ajSS ", fg_color='black', anchor=tk.CENTER)
+        self.my_text.place(relx=0.5, rely=0.95)
+
 
     def button_function(self, _inf):
-        print(_inf.SCREEN_WIDTH)
+        self.my_text.configure(text="gggggggg")
